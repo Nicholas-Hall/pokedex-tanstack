28cae272a0e7fbd39826bee83048fa4b792dc622
Nicholas Hall <nickjohnhall@gmail.com>
2025-03-01 14:30:44 -0600

    dont down dev tools in prod

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -1,22 +1,22 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const DEV = process.env.NODE_ENV === "development"; // Check if in development mode
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{' '}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
+        <Link to="/" className="[&.active]:font-bold">Home</Link>{" "}
+        <Link to="/about" className="[&.active]:font-bold">About</Link>
       </div>
       <hr />
       <Outlet />
-      <TanStackRouterDevtools />
-      <ReactQueryDevtools initialIsOpen={false} />
+      
+      {/* Only show DevTools in development */}
+      {DEV && <TanStackRouterDevtools />}
+      {DEV && <ReactQueryDevtools initialIsOpen={false} />}
     </>
   ),
-})
+});
